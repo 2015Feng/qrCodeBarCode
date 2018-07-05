5c52fa6b8fc400e1539cc71857d7bd9a69404070
2015Feng <r41r40@gmail.com>
2018-07-05 13:52:09 +0800

    添加检查更新功能

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
+const updateManager = wx.getUpdateManager()
+
 //app.js
 App({
   onLaunch: function () {
-    
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      console.log(res.hasUpdate)
+    })
+
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function (res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+
+    updateManager.onUpdateFailed(function () {
+      // 新的版本下载失败
+    })
   },
   globalData: {
     userInfo: null
